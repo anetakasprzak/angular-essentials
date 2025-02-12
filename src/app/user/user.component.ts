3539304e243141cfc33e15e3df10ae6c0b667837
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-02-12 13:04:12 +0000

    model.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,12 @@
 import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
-
+import { type User } from './user.model';
 // type User = {
 //   id: string;
 //   avatar: string;
 //   name: string;
 // }
 
-interface User {
-    id: string;
-  avatar: string;
-  name: string;
-}
+
 
 @Component({
   selector: 'app-user',
