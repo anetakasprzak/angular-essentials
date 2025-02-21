cd4b679c475867a3cef2a633e4556cd71ea20155
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-02-21 14:00:59 +0000

    ng-content

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 // type User = {
 //   id: string;
 //   avatar: string;
@@ -11,7 +12,7 @@ import { type User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
