457a94c6a00afa7df718e86d978ae8881e587843
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-04-16 10:02:06 +0100

    Migrating All Components To Use Modules

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
+
 // type User = {
 //   id: string;
 //   avatar: string;
@@ -11,10 +11,8 @@ import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  imports: [CardComponent],
 })
 
 export class UserComponent {
